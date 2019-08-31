ff517af539f3f938181815d770a318ff0188aad3
Amar Singh <nly@disroot.org>
2019-08-31 08:42:06 +0530

    mru: add proc delq1, add accessor mru-list, cleanup

--- emacsy/mru-stack.scm
+++ emacsy/mru-stack.scm
@@ -35,40 +35,51 @@
   #:use-module (oop goops)
   #:use-module (emacsy util)
   #:use-module (srfi srfi-1)
-  #:use-module (srfi srfi-26)
   #:export (<mru-stack>
+            ;; accessor
+            mru-list
+            ;; procs
             mru-add
             mru-remove
             mru-recall
             mru-set
             mru-ref
+            mru-other
             mru-empty?
             mru-contains?
             mru-next
             mru-prev
-            mru->list))
+            ))
 
 ;;; Return on Failure is an error
 (define LIST-BEGIN 0)
 (define INCR 1)
 
+(define (delq1 item lst)
+  (if (null? lst) lst
+      (let ((elt (car lst)))
+        (if (eq? item elt)
+            (cdr lst)
+            (cons elt (delq1 item (cdr lst)))))))
+
 (define-class <mru-stack> ()
-  (queue #:accessor q
-         #:init-keyword #:q
-         #:init-thunk (lambda () (list))))
+  (list #:accessor mru-list #:init-keyword #:list #:init-thunk (lambda () (list))))
 
 (define-method (write (obj <mru-stack>) port)
-  (format port "<mru-stack ~a>" (mru->list obj)))
+  (format port "<mru-stack ~a>" (mru-list obj)))
 
-;;; mru is quite effectful, this might become an issue. make composable
-;;; alternatives to mru-X! -> mru-X
+(define-method (mru-empty? (s <mru-stack>))
+  (null-list? (mru-list s)))
+
+(define-method (mru-contains? (s <mru-stack>) x)
+  (list-index (lambda (y) eq? y x) (mru-list s)))
+
 (define-method (mru-add (s <mru-stack>) x)
-  (make <mru-stack> #:q (xcons (q s) x)))
+  (make <mru-stack> #:list (xcons (mru-list s) x)))
 
 (define-method (mru-remove (s <mru-stack>) item)
-  (make <mru-stack> #:q (delq1! item (list-copy (q s)))))
+  (make <mru-stack> #:list (delq1 item (mru-list s))))
 
-;;; note: easily the most important proc
 (define-method (mru-recall (s <mru-stack>) item)
   (if (mru-contains? s item)
       (mru-add (mru-remove s item) item)
@@ -83,21 +94,12 @@
 (define-method* (mru-ref (s <mru-stack>) #:optional (ref LIST-BEGIN))
   (if (mru-empty? s)
       (error (format #f "In procedure mru-ref: Empty Mru-stack ~s" s))
-      (if (>= ref (length (mru->list s)))
-          (error (format #f "In procedure mru-ref: Ref ~s is larger than Mru-stack of length ~s" s (length (q s))))
-          (list-ref (mru->list s) ref))))
+      (if (>= ref (length (mru-list s)))
+          (error (format #f "In procedure mru-ref: Ref ~s not found in ~s of length ~s" ref s (length (mru-list s))))
+          (list-ref (mru-list s) ref))))
 
-(define-method (mru->list (s <mru-stack>))
-  (q s))
-
-(define-method (->list (s <mru-stack>))
-  (q s))
-
-(define-method (mru-empty? (s <mru-stack>))
-  (null-list? (q s)))
-
-(define-method (mru-contains? (s <mru-stack>) x)
-  (list-index (cut eq? <> x) (mru->list s)))
+(define-method* (mru-other (s <mru-stack>) #:optional (ref INCR))
+  (mru-recall s (mru-ref s ref)))
 
 (define (circular-list->list q)
   (define (clst->list start q)
@@ -110,10 +112,10 @@
 (define-method* (mru-next (s <mru-stack>) #:optional (count INCR))
   (if (mru-empty? s) s
       (make <mru-stack>
-        #:q (let ((proc (if (negative? count) reverse identity))
+        #:list (let ((proc (if (negative? count) reverse identity))
                   (count (if (negative? count) (- count) count)))
               (proc (circular-list->list (drop (apply circular-list
-                                                      (proc (mru->list s)))
+                                                      (proc (mru-list s)))
                                                count)))))))
 
 (define-method* (mru-prev (s <mru-stack>) #:optional (count INCR))
